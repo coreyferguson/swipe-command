b621e5702da936cdf3764055c05a6333a29e163c
Corey Ferguson <corey_ferguson@intuit.com>
2017-10-26 16:51:39 -0700

    upload to aws s3 bucket

--- scripts/dist.js
+++ scripts/dist.js
@@ -2,29 +2,91 @@
 /* eslint no-console: "off" */
 
 const asciify = require('asciify');
+const AWS = require('aws-sdk');
 const path = require('path');
+const S3Client = require('s3');
 const { spawn } = require('child-process-promise');
+
+// ///////////////// //
+// AWS configuration //
+// ///////////////// //
+
+AWS.config.update({
+  region: 'us-west-2'
+});
+const s3 = new AWS.S3({apiVersion: '2006-03-01'});
+const client = S3Client.createClient({ s3Client: s3 });
+const bucketName = 'swipe-command.overattribution.com';
+
+// //////////////////// //
+// High level execution //
+// //////////////////// //
 
 cleanDistFolder()
   .then(copyPublicAssets)
   .then(webpack)
-  .then(() => {
-    return new Promise((resolve, reject) => {
-      asciify('Success!', { font: 'shadow' }, (err, res) => {
-        if (err) reject(err);
-        else resolve(res);
-      });
-    });
-  })
-  .then(message => {
-    console.log();
-    console.log(message);
+  .then(createBucket)
+  .then(applyWebsiteHosting)
+  .then(applyPolicy)
+  .then(uploadDist)
+  .then(printSuccess)
+  .catch(err => {
+    if (err.code === 'BucketAlreadyOwnedByYou') return uploadDist();
+    else throw err;
   })
   .catch(err => {
     if (err && err.message) console.error('message:', err.message);
     if (err && err.stderr) console.error('stderr:', err.stderr);
     if (err && err.stack) console.error('stack:', err.stack);
   });
+
+/////////////////////////
+// Low level execution //
+/////////////////////////
+
+function applyPolicy() {
+  console.log('Applying policy to S3 bucket.');
+  return new Promise((resolve, reject) => {
+    s3.putBucketPolicy({
+      Bucket: bucketName,
+      Policy: `{
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Sid": "PublicReadForGetBucketObjects",
+                "Effect": "Allow",
+                "Principal": "*",
+                "Action": "s3:GetObject",
+                "Resource": "arn:aws:s3:::${bucketName}/*"
+            }
+        ]
+      }`
+    }, (err, data) => {
+      if (err) reject(err);
+      else resolve(data);
+    });
+  });
+}
+
+function applyWebsiteHosting() {
+  console.log('Applying static website hosting to S3 bucket.');
+  return new Promise((resolve, reject) => {
+    s3.putBucketWebsite({
+      Bucket: bucketName,
+      WebsiteConfiguration: {
+        ErrorDocument: {
+          Key: 'error.html'
+        },
+        IndexDocument: {
+          Suffix: 'index.html'
+        }
+      }
+    }, (err, data) => {
+      if (err) reject(err);
+      else resolve(data);
+    });
+  });
+}
 
 function cleanDistFolder() {
   console.log('Cleaning dist folder.');
@@ -51,6 +113,30 @@ function copyPublicAssets() {
   );
 }
 
+function createBucket() {
+  console.log('Creating S3 bucket.');
+  return new Promise((resolve, reject) => {
+    s3.createBucket({
+      Bucket: bucketName
+    }, (err, data) => {
+      if (err) reject(err);
+      else resolve(data);
+    });
+  });
+}
+
+function printSuccess() {
+  return new Promise((resolve, reject) => {
+    asciify('Success!', { font: 'shadow' }, (err, res) => {
+      if (err) reject(err);
+      else resolve(res);
+    });
+  }).then(message => {
+    console.log();
+    console.log(message);
+  });
+}
+
 function webpack() {
   console.log('Building src with webpack.');
   return spawn(
@@ -59,3 +145,19 @@ function webpack() {
     { capture: [ 'stdout', 'stderr' ]}
   );
 }
+
+function uploadDist() {
+  console.log('Uploading dist folder to AWS.');
+  return new Promise((resolve, reject) => {
+    const uploader = client.uploadDir({
+      localDir: 'dist',
+      deleteRemoved: true,
+      s3Params: {
+        Bucket: bucketName
+      }
+    });
+    uploader.on('error', reject);
+    uploader.on('end', resolve);
+  });
+}
+
